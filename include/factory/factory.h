94cca27ac4c6aaa025bdfeb3d8d94bcc79728d93
pushecx <philipp.hetfeld@tu-dortmund.de>
2018-11-03 22:44:42 +0100

    Implementation + Tests + Documentation Generation

--- include/factory/factory.h
+++ include/factory/factory.h
@@ -1,8 +1,116 @@
 #pragma once
+#include <array>
+#include <memory>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
 
-
-class factory {
+/// <summary>
+/// Internal Class for Storing either a std::unique_ptr or the Class.
+/// </summary>
+template <typename Interface, typename Key>
+class factory_internal {
  public:
-  factory() = default;
-  ~factory() = default;
+  factory_internal() = default;
+  ~factory_internal() = default;
+
+ protected:
+  using StorageType = Interface;
+  std::unordered_map<Key, Interface> m_map;
+};
+/// <summary>
+/// FWD Declaration of the Factory you should use!
+/// </summary>
+template <class Interface, class Key = std::string_view, typename Enable = void>
+class factory;
+/// <summary>
+/// If the Interface is abstract, this Implementation is choosen
+/// </summary>
+template <class Interface, class Key>
+class factory<Interface, Key, typename std::enable_if_t<std::is_abstract_v<Interface>>> : public factory_internal<std::unique_ptr<Interface>, Key> {
+ public:
+  /// <summary>
+  /// Registering a new Type to return a Copy from at copy. Throws if the key is already used. TODO: Find a better Name!
+  /// </summary>
+  /// <param name="key">Key to  identify the new Type</param>
+  /// <param name="arguments">Arguments to create the new Type with std::make_unique</param>
+  template <class T, class... Args>
+  void register_(const Key& key, Args&&... arguments) {
+    // Check if Key exists
+    if (this->m_map.find(key) == this->m_map.end()) {
+      this->m_map[key] = std::make_unique<T>(std::forward<Args>(arguments)...);
+    } else {
+      throw std::runtime_error("Duplicate Key");
+    }
+  }
+  /// <summary>
+  /// Method to create a Copy of the Data Type stored associated with Key. Throws if the key isn't found!
+  /// </summary>
+  /// <param name="Key">Key of the Type</param>
+  /// <returns>Copy of the Type stored under Key</returns>
+  template <class T>
+  T copy(const Key& key) const {
+    if (this->m_map.find(key) == this->m_map.end()) {
+      throw std::runtime_error("Key not found");
+    } else {
+      return *(static_cast<T*>(this->m_map.at(key).get()));
+    }
+  }
+};
+/// <summary>
+/// Factory Implementation if Interface isn't abstract, be careful about Object Slicing
+/// </summary>
+template <class Interface, class Key>
+class factory<Interface, Key, typename std::enable_if_t<!std::is_abstract_v<Interface>>> : public factory_internal<Interface, Key> {
+ public:
+  /// <summary>
+  /// Registering a new Type with a Key. Throws if the Key already exists
+  /// </summary>
+  /// <param name="key">Key to use</param>
+  /// <param name="Args">Arguments to use, to instaniate the new Type</param>
+  template <typename T = Interface, class... Args>
+  void register_(const Key & key, Args&&... arguments) {
+    if (this->m_map.find(key) != this->m_map.end()) {
+      throw std::runtime_error("Key already exists");
+    } else {
+      this->m_map[key] = T{std::forward<Args>(arguments)...};
+    }
+  }
+  /// <summary>
+  /// Method to create a Copy from a Type. Template Parameter can be used to invoke a static_cast.
+  /// </summary>
+  /// <param name="key">Key of the Type</param>
+  template <class T = Interface>
+  T copy(const Key & key) {
+    return static_cast<T>(this->m_map.at(key));
+  }
+};
+/// <summary>
+/// Constexpr Factory
+/// </summary>
+template <class Interface, std::size_t Size, class Key = std::string_view>
+class constexpr_factory {
+ public:
+  constexpr_factory() = delete;
+  /// <summary>
+  /// Constructor to create a new Factory.
+  /// </summary>
+  /// <param name="arguments">Template Pack, must be convertible to std::pair<Key,Interface></param>
+  template <class... Args>
+  constexpr constexpr_factory(Args&&... arguments) : data{std::forward<Args>(arguments)...} {
+    static_assert(Size == sizeof...(arguments));
+  }
+  /// <summary>
+  /// Function to create a Copy from a registered Type
+  /// </summary>
+  Interface copy(const Key& key) const {
+    for (std::size_t i = 0; i < Size; i++) {
+      if (key == data[i].first)
+        return data[i].second;
+    }
+    throw std::runtime_error("Key not found");
+  }
+
+ private:
+  const std::array<std::pair<Key, Interface>, Size> data;
 };
--- tests/factory_tests.cpp
+++ tests/factory_tests.cpp
@@ -1,1 +1,101 @@
+#pragma warning(push)
+#pragma warning(disable : 4251)
+#pragma warning(disable : 4275)
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <gtest/gtest.h>
+#undef _CRT_SECURE_NO_WARNINGS
+#pragma warning(pop)
+#include <factory/factory.h>
+
+void tell_methods(std::string_view view) {
+  constexpr bool be_noisy = true;
+  if constexpr (be_noisy) {
+    std::cout << view.data() << std::endl;
+  }
+}
+enum class test_enum : uint_fast32_t {
+  first = 1,
+  second = 2,
+  third = 3,
+};
+
+class noisy_base_abstract {
+ public:
+  virtual int do_stuff() = 0;
+};
+class noisy_base : public noisy_base_abstract {
+ public:
+  noisy_base() { tell_methods("noisy_base()"); }
+  noisy_base(const noisy_base&) { tell_methods("noisy_base(const noisy_base&)"); }
+  noisy_base(noisy_base&&) { tell_methods("noisy_base(noisy_base&&)"); }
+  noisy_base& operator=(const noisy_base&) {
+    tell_methods("operator=(const noisy_base&)");
+    return *this;
+  }
+  noisy_base& operator=(noisy_base&&) {
+    tell_methods("operator=(noisy_base&&)");
+    return *this;
+  }
+  ~noisy_base() { tell_methods("~noisy_base()"); }
+  int do_stuff() {
+    tell_methods("noisy_base::do_stuff()");
+    return 0;
+  };
+};
+struct noisy1 : public noisy_base {
+  int do_stuff() {
+    tell_methods("noisy1::do_stuff()");
+    return 1;
+  }
+  ~noisy1() { tell_methods("~noisy1()"); }
+};
+struct noisy2 : public noisy_base {
+  virtual int do_stuff() {
+    tell_methods("noisy2::do_stuff()");
+    return 2;
+  }
+  virtual ~noisy2() { tell_methods("~noisy2()"); }
+};
+struct noisy3 : public noisy_base {
+  virtual int do_stuff() {
+    tell_methods("noisy3::do_stuff()");
+    return 3;
+  }
+  virtual ~noisy3() { tell_methods("~noisy3()"); }
+};
+
+constexpr constexpr_factory<int, 2, test_enum> compile_time_factory = {std::make_pair<test_enum, int>(test_enum::first, 1),
+                                                                       std::make_pair<test_enum, int>(test_enum::second, 2)};
+
+TEST(factory, simple_key) {
+  factory<int> test_factory{};
+  auto key = std::string_view{"first_key"};
+  test_factory.register_(key, 5);
+  auto temp = test_factory.copy(key);
+  ASSERT_EQ(temp, 5);
+}
+
+TEST(factory, noisy) {
+  factory<noisy_base, test_enum> my_factory;
+  my_factory.register_<noisy1>(test_enum::first);
+  my_factory.register_<noisy2>(test_enum::second);
+  my_factory.register_<noisy3>(test_enum::third);
+  //Object Slicing, If you want you can static_cast it with the template Parameter of copy
+  ASSERT_EQ(my_factory.copy(test_enum::first).do_stuff(), 0);
+  ASSERT_EQ(my_factory.copy(test_enum::second).do_stuff(), 0);
+  ASSERT_EQ(my_factory.copy(test_enum::third).do_stuff(), 0);
+}
+
+TEST(factory, noisy_abstract) {
+  factory<noisy_base_abstract, test_enum> my_factory;
+  my_factory.register_<noisy1>(test_enum::first);
+  my_factory.register_<noisy2>(test_enum::second);
+  my_factory.register_<noisy3>(test_enum::third);
+  ASSERT_EQ(my_factory.copy<noisy1>(test_enum::first).do_stuff(), static_cast<int>(test_enum::first));
+  ASSERT_EQ(my_factory.copy<noisy2>(test_enum::second).do_stuff(), static_cast<int>(test_enum::second));
+  ASSERT_EQ(my_factory.copy<noisy3>(test_enum::third).do_stuff(), static_cast<int>(test_enum::third));
+}
+
+TEST(factory, compile_time_factory) {
+  ASSERT_EQ(1, compile_time_factory.copy(test_enum::first));
+}
